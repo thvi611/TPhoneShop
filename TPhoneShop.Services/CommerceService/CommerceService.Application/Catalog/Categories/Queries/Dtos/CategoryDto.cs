23c6120df66a866ad4f779ae317941275d4e5d23
thvi611 <lovivivi611@gmail.com>
2026-09-10 22:36:18 +0700

    feat(api): implement get categories

---
+++ TPhoneShop.Services/CommerceService/CommerceService.Application/Catalog/Categories/Queries/Dtos/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CommerceService.Application.Catalog.Categories.Queries.Dtos
+{
+    public class CategoryDto
+    {
+        public Guid? Id { get; set; }
+        public Guid? ParentId { get; set; }
+        public required string Name { get; set; }
+        public required string Slug { get; set; }
+        public required string Description { get; set; }
+    }
+}
---
+++ TPhoneShop.Services/CommerceService/CommerceService.Application/Catalog/Categories/Queries/GetCategoriesForAdmin/GetCategoriesForAdminQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Infrastructure.Extensions;
+using CommerceService.Application.Catalog.Categories.Queries.Dtos;
+using CommerceService.Application.Common.Abstractions;
+
+namespace CommerceService.Application.Catalog.Categories.Queries.GetCategoriesForAdmin
+{
+    internal class GetCategoriesForAdminQueryHandler(CommerceDbContext dbContext, IMediaService mediaService) : IRequestHandler<GetCategoriesForAdminQuery, PagingResponse<CategoryForAdminDto>>
+    {
+        public async Task<PagingResponse<CategoryForAdminDto>> Handle(GetCategoriesForAdminQuery request, CancellationToken cancellationToken)
+        {
+            var query = dbContext.Categories
+                                 .AsNoTracking()
+                                 .WhereIf(!string.IsNullOrEmpty(request.Search), e => EF.Functions.ILike(e.Name, $"%{request.Search}%"))
+                                 .Where(e => e.IsActive == request.IsActive)
+                                 .OrderBy(e => e.Name)
+                                 .Select(e => new
+                                 {
+                                     e.Id,
+                                     e.ParentId,
+                                     e.Name,
+                                     e.Slug,
+                                     e.Description,
+                                     e.IsActive
+                                 });
+            var totalCount = await query.CountAsync(cancellationToken);
+            var categorys = await query.Paginate(request.PageNumber, request.PageSize)
+                                    .ToListAsync(cancellationToken);
+
+            var items = await Task.WhenAll(categorys.Select(async category => new CategoryForAdminDto
+            {
+                Id = category.Id,
+                ParentId = category.ParentId,
+                Name = category.Name,
+                Slug = category.Slug,
+                Description = category.Description,
+                IsActive = category.IsActive
+            }));
+
+            return new PagingResponse<CategoryForAdminDto>
+            {
+                TotalCount = totalCount,
+                Items = items
+            };
+        }
+    }
+}
